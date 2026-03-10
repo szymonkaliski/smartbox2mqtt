fe728a6d2e0c69a1275d30b216c3252d96f5ff7c
Szymon Kaliski <hi@szymonkaliski.com>
2026-03-10 19:32:43 +0100

    add nix flake dev shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.nodejs_22 ];
+        };
+      });
+    };
+}
